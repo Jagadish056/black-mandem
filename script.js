c336eae3759542ecea5423675983afd8e876bebb
Jagadish Sharma <sharma.jagadish056@gmail.com>
2025-09-21 20:32:38 +0545

    save

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
+if (!Array.prototype.unique) {
+    Array.prototype.unique = function() {
+        return [...new Set(this)]
+    }
+}
+
+if (!Array.prototype.pluck) {
+    Array.prototype.pluck = function(field) {
+        return this.map(obj => obj[field])
+    }
+}
+
+if (!Array.prototype.sortByDate) {
+    Array.prototype.sortByDate = function(field) {
+        return this.sort((a, b) => new Date(b[field]) - new Date(a[field]))
+    }
+}
+
 document.addEventListener('alpine:init', () => {
 
     Alpine.store('dev', window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
 
-    Alpine.store('gtav', {
-        data: {},
-
-        init() {
-            this.getData()
+    Alpine.store('api', {
+        baseUrl:"",
+        data:{},
+        async init () {
+            await axios.get(this.baseUrl + "/gtav.json")
+            .then(res => res.data.data)
             .then(res => {
-                res.blackMandem?.history.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp))
-                this.data = res
+                res?.history.sortByDate('timestamp')
+                this.data = res ?? {}
             })
             .catch(err => console.error('Failed to load:', err))
-        },
-
-        async getData() {
-            return await axios.get(
-                Alpine.store('dev') ? "/gtav.json" : "https://cdn.jsdelivr.net/gh/Jagadish056/black-mandem@main/gtav.json"
-            ).then(res => res.data)
         }
-    })
+    } )
 
     Alpine.data('hero', () => ({
         loading: true,
-        backgroundImages: [
-            "./images/hood.jpeg",
-            "./images/1.png",
-            "./images/2.png",
-            "./images/3.png"
-        ],
-        activeBackgroundImage:0,
+        activeWallpaper:0,
         init() {
             
 
@@ -37,12 +44,12 @@ document.addEventListener('alpine:init', () => {
 
                 if (!this.loading) {
                     this.loading = true
-                    this.activeBackgroundImage = 0
+                    this.activeWallpaper = 0
                 } else {
-                    if (this.activeBackgroundImage >= (this.backgroundImages.length - 1)) {
+                    if (this.activeWallpaper >= (this.$store.api.data.wallpaper?.length - 1)) {
                         this.loading = false
                     } else {
-                        this.activeBackgroundImage++
+                        this.activeWallpaper++
                     }
                 }
             }, 8000)
@@ -52,4 +59,18 @@ document.addEventListener('alpine:init', () => {
             this.open = ! this.open
         }
     }))
+
+    Alpine.data('app', () => ({
+        init() {
+            this.$store.api.baseUrl = Alpine.store('dev') ? "" : "https://cdn.jsdelivr.net/gh/Jagadish056/black-mandem@main"
+            this.$store.api.init()
+
+            // this.$nextTick(() => {
+            //     console.log(this.$store.api.data)
+            // });
+        },
+
+        
+    }))
+
 })
